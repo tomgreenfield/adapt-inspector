9a66be814b5df5531f42e709a39d8717ba76a1c1
tomgreenfield <tomgreenfield@users.noreply.github.com>
2014-06-20 17:41:27 +0100

    Committed files

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-adapt-inspector
-===============
+# adapt-inspector
 
 An extension to inspect details of elements on a page. Reviewers may click the element IDs to create tickets on Trac.
+
+## Installation
+
+* Add the following JSON to `config.json`:
+```json
+"_inspector": {
+	"_isEnabled": true,
+	"_tracUrl": ""
+}
+```
+
+* Toggle `_isEnabled` to turn Inspector on or off.
+* Populate `_tracUrl` to make element IDs clickable to a specific Trac.
---
+++ js/adapt-inspector.js
@@ -0,0 +1,102 @@
+/*
+* Inspector
+* License - https://github.com/adaptlearning/adapt_framework/blob/master/LICENSE
+* Maintainers - Tom Greenfield
+*/
+
+define(function(require) {
+
+	var Adapt = require("coreJS/adapt");
+	var Backbone = require("backbone");
+
+	var InspectorView = Backbone.View.extend({
+
+		initialize: function() {
+			this.listenTo(Adapt, "remove", this.remove);
+			this.render();
+		},
+
+		events: {
+			"click .inspector-inner": "onClick",
+			"mouseenter": "onEnter",
+			"mouseleave": "onLeave",
+		},
+
+		render: function() {
+			var data = this.model.toJSON();
+			var template = Handlebars.templates["inspector"];
+			
+			this.$el.append(template(data));
+			return this;
+		},
+
+		onClick: function(event) {
+			var tracUrl = Adapt.config.get("_inspector")._tracUrl;
+			var clickedId = this.model.get("_id");
+			var clickedTitle = this.model.get("displayTitle");
+			var clickedLocation = Adapt.location._currentId;
+			var params = clickedId;
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			if (!tracUrl) return console.log("No _tracUrl defined in config.json.");
+			if (clickedTitle) params += " " + clickedTitle;
+			if (clickedId != clickedLocation) params += " (" + Adapt.location._contentType + " " + clickedLocation + ")";
+
+			window.open(tracUrl + "/newticket?summary=" + params);
+		},
+
+		onEnter: function() {
+			this.$el.addClass("inspector-active");
+			this.setVisibility();
+		},
+
+		onLeave: function() {
+			this.$el.removeClass("inspector-active");
+			this.setVisibility();
+		},
+
+		setVisibility: function() {
+			var hoveredElement;
+
+			$(".inspector-container").removeClass("inspector-visible");
+			$(".inspector").on("transitionend", function() { $(this).hide(); });
+
+			if ($(".component.inspector-active:hover").length !== 0) {
+				hoveredElement = ".component.inspector-active";
+			} else if ($(".block.inspector-active:hover").length !== 0) {
+				hoveredElement = ".block.inspector-active";
+			} else if ($(".article.inspector-active:hover").length !== 0) {
+				hoveredElement = ".article.inspector-active";
+			} else if ($(".page.inspector-active:hover").length !== 0) {
+				hoveredElement = ".page.inspector-active";
+			} else if ($(".menu-item.inspector-active:hover").length !== 0) {
+				hoveredElement = ".menu-item.inspector-active";
+			} else if ($(".menu.inspector-active:hover").length !== 0) {
+				hoveredElement = ".menu.inspector-active";
+			}
+			else return;
+
+			$(hoveredElement + " > .inspector").off().show(0, function() {
+				$(hoveredElement).addClass("inspector-visible");
+			});
+		}
+
+	});
+
+	function addInspector(view) {
+		new InspectorView({el: view.$el, model: view.model});
+	}
+
+	Adapt.on("app:dataReady", function() {
+		if (!Adapt.device.touch && Adapt.config.get("_inspector") && Adapt.config.get("_inspector")._isEnabled) {
+			Adapt.on("menuView:postRender pageView:postRender articleView:postRender blockView:postRender componentView:postRender", function(view) {
+				addInspector(view);
+				view.$el.addClass("inspector-container");
+				if (!Adapt.config.get("_inspector")._tracUrl) $(".inspector-inner").css("cursor", "default");
+			});
+		}
+	});
+
+});
